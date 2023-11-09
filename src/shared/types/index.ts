b3ecae4e8218c7ebba3a70c2007ef8e00c55ef4f
monikahuenika <91833875+monikahuenika@users.noreply.github.com>
2023-11-09 19:22:22 +0500

    add logger and Rest Config

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,4 +1,5 @@
 export * from './comment.type.js';
+export * from './component.enum.js';
 export * from './offer.type.js';
 export * from './user.type.js';
 export * from './mock-server-data.type.js';
