0cd3b207a51b637d6837e87cd9d74bf7c22106ea
monikahuenika <91833875+monikahuenika@users.noreply.github.com>
2023-11-09 07:26:36 +0500

    add generate and mock stuff

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,13 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  cities: string[];
+  previewImages: string[];
+  offerImages: string[];
+  housingTypes: string[];
+  options: string[];
+  userNames: string[];
+  emails: string[];
+  avatars: string[];
+  passwords: string[];
+};
