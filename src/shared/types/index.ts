a800ee9323a334ee32517ffeedcd89b7858cb8b0
monikahuenika <91833875+monikahuenika@users.noreply.github.com>
2023-12-15 20:37:10 +0500

    Add middleware

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -3,3 +3,4 @@ export * from './component.enum.js';
 export * from './offer.type.js';
 export * from './user.type.js';
 export * from './mock-server-data.type.js';
+export * from './document-exists.interface.js';
